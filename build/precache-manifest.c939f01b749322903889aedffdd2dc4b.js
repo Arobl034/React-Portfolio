dd0d5ba12df90abff934d870f5c4f81db8bbc39d
Arobl034 <robles305@gmail.com>
2020-06-07 21:55:53 -0400

    build

---
+++ build/precache-manifest.c939f01b749322903889aedffdd2dc4b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d785626b84e4efb43f29a348a3d563c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "419d8cbf7c84563c8ce4",
+    "url": "/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "756ddcf517215bd1de7e",
+    "url": "/static/css/main.34205297.chunk.css"
+  },
+  {
+    "revision": "419d8cbf7c84563c8ce4",
+    "url": "/static/js/2.93cea50f.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/static/js/2.93cea50f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "756ddcf517215bd1de7e",
+    "url": "/static/js/main.d602c906.chunk.js"
+  },
+  {
+    "revision": "2490790c3d9e824a7faa",
+    "url": "/static/js/runtime-main.18bbefb9.js"
+  },
+  {
+    "revision": "1957a190e165f03145cee2c0d5e17dd7",
+    "url": "/static/media/Project1.1957a190.png"
+  },
+  {
+    "revision": "4709f9bb56f297cf3b0f6cb4f46d5736",
+    "url": "/static/media/Project2.4709f9bb.png"
+  },
+  {
+    "revision": "5a373c596bbbbadcf9df4291f77b8c43",
+    "url": "/static/media/btracker.5a373c59.png"
+  },
+  {
+    "revision": "16514a0990b5a3898db2d8ca8db6e414",
+    "url": "/static/media/eatdaburger.16514a09.png"
+  },
+  {
+    "revision": "db45bb03f6872f1f88374fa79bd7b0d7",
+    "url": "/static/media/ftracker.db45bb03.png"
+  },
+  {
+    "revision": "c7407e7544a7be7575c4437e609d84f7",
+    "url": "/static/media/notetaker.c7407e75.png"
+  }
+]);
