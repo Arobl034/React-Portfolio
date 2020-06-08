cdf4cc2bfbbba3e09c84ad5d2b4276a4705aef91
Arobl034 <robles305@gmail.com>
2020-06-07 21:16:59 -0400

    project page

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,141 +1,85 @@
-import React, { Component } from "react";
-import {
-  Tabs,
-  Tab,
-  Card,
-  Grid,
-  CardTitle,
-  Button,
-  CardText,
-  CardMenu,
-  CardActions,
-  IconButton,
-  Cell,
-} from "react-mdl";
+import React, { } from "react";
 
-class Projects extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { activeTab: 0 };
-  }
 
-  toggleCategories() {
-    if (this.state.activeTab === 0) {
-      return (
-        <div className="projects-grid">
-          {/* Project 1 */}
-          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
-            <CardTitle
-              style={{
-                color: "#fff",
-                height: "176px",
-                background:
-                  "url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover",
-              }}
-            >
-              React Project #1
-            </CardTitle>
-            <CardText>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's
-            </CardText>
-            <CardActions border>
-              <Button colored>GitHub</Button>
-              <Button colored>Heroku</Button>
-            </CardActions>
-            <CardMenu style={{ color: "#fff" }}>
-              <IconButton name="share" />
-            </CardMenu>
-          </Card>
-
-          {/* Project 2 */}
-          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
-            <CardTitle
-              style={{
-                color: "#fff",
-                height: "176px",
-                background:
-                  "url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover",
-              }}
-            >
-              React Project #2
-            </CardTitle>
-            <CardText>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's
-            </CardText>
-            <CardActions border>
-              <Button colored>GitHub</Button>
-              <Button colored>Heroku</Button>
-            </CardActions>
-            <CardMenu style={{ color: "#fff" }}>
-              <IconButton name="share" />
-            </CardMenu>
-          </Card>
-
-          {/* Project 3 */}
-          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
-            <CardTitle
-              style={{
-                color: "#fff",
-                height: "176px",
-                background:
-                  "url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover",
-              }}
-            >
-              React Project #3
-            </CardTitle>
-            <CardText>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's
-            </CardText>
-            <CardActions border>
-              <Button colored>GitHub</Button>
-              <Button colored>Heroku</Button>
-            </CardActions>
-            <CardMenu style={{ color: "#fff" }}>
-              <IconButton name="share" />
-            </CardMenu>
-          </Card>
-        </div>
-      );
-    } else if (this.state.activeTab === 1) {
-      return (
+function Projects() {
+  return (
+    <div>
+      <h1 className="titlePage"></h1>
+      <section>
         <div>
-          <h1>MongoDB</h1>
+          <div className="row">
+            <div className="col-lg-3 col-md-4">
+              <h2 className="Project1">Weather Fit</h2>
+              <a href="https://mmmasrieh.github.io/Crew/">
+                <img
+                  src={require("../assets/Project1.png")}
+                  height="200px"
+                  width="200px"
+                  alt=""
+                />
+              </a>
+            </div>
+            <div className="col-lg-3 col-md-4">
+              <h2 className="Project1">Note taker</h2>
+              <a href="https://obscure-harbor-39963.herokuapp.com">
+                <img
+                  src={require("../assets/notetaker.png")}
+                  height="200px"
+                  width="200px"
+                  alt=""
+                />
+              </a>
+            </div>
+            <div className="col-lg-3 col-md-4">
+              <h2 className="Project1">Fitness Tracker</h2>
+              <a href="https://thawing-citadel-06698.herokuapp.com/?id=5eb8a15487a0f3001712072d">
+                <img
+                  src={require("../assets/ftracker.png")}
+                  height="200px"
+                  width="200px"
+                  alt=""
+                />
+              </a>
+            </div>
+            <div className="col-lg-3 col-md-4">
+              <h2 className="Project1">Budget Tracker</h2>
+              <a href="https://peaceful-brook-52779.herokuapp.com">
+                <img
+                  src={require("../assets/btracker.png")}
+                  height="200px"
+                  width="200px"
+                  alt=""
+                />
+              </a>
+            </div>
+            <div className="col-lg-3 col-md-4">
+              <h2 className="Project1">Eat-Da-Burger</h2>
+              <a href="https://arobl034.github.io/EatDaBurger.github.io/">
+                <img
+                  src={require("../assets/eatdaburger.png")}
+                  height="200px"
+                  width="200px"
+                  alt=""
+                />
+              </a>
+            </div>
+            <div className="col-lg-3 col-md-4">
+              <h2 className="Project1">Local One Off</h2>
+              <a href="https://local-one-off.herokuapp.com/">
+                <img
+                  src={require("../assets/Project2.png")}
+                  height="200px"
+                  width="200px"
+                  alt=""
+                />
+              </a>
+            </div>
+          </div>
         </div>
-      );
-    } else if (this.state.activeTab === 2) {
-      return (
-        <div>
-          <h1>Node</h1>
-        </div>
-      );
-    }
-  }
-  render() {
-    return (
-      
-      <div className="category-tabs">
-        <Tabs
-          activeTab={this.state.activeTab}
-          onChange={(tabId) => this.setState({ activeTab: tabId })}
-          ripple
-        >
-          <Tab>React</Tab>
-          <Tab>MongoDB</Tab>
-          <Tab>Node</Tab>
-        </Tabs>
-        <Grid>
-          <Cell col={12}>
-            <div className="content">{this.toggleCategories()}</div>
-          </Cell>
-        </Grid>
-        <div className="project-body"></div>
-      </div>
-      
-    );
-  }
+      </section>
+      <br />
+    </div>
+  );
 }
 
 export default Projects;
